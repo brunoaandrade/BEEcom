53a73b45a237891553fa0b7128ed7fc14b537b90
dpacheco <david.pacheco@beubi.com>
2015-11-11 16:50:20 +0000

    BEEWEB-7 fix for beeweb daemon to start

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ INSTALL_REQUIRES = ["pyusb"]
 from distutils.core import setup
 
 setup(name='beecom',
-      version='0.2',
+      version='0.2.1',
       description='BVC Printer Python driver',
       long_description=open("README.md").read(),
       author="BVC Electronic Systems",
